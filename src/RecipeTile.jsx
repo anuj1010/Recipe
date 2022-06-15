e0c924dbaaae810c6e9ee991a2138c3466b045e2
Anuj Singh <editngwithanuj@gmail.com>
2022-06-15 15:45:46 +0530

    fix1.4

--- src/RecipeTile.jsx
+++ src/RecipeTile.jsx
@@ -6,7 +6,7 @@ function RecipeTile({recipe}) {
         <div className="tiles">
             <img className='tile_image' alt='recipe' src={recipe["recipe"]["image"]} />
             <p className='tile_title'>{recipe["recipe"]["label"]}</p>
-            <a className='recipe_button' href={recipe["recipe"]["shareAs"]} target="_blank">Get Recipe</a>
+            <a className='recipe_button' href={recipe["recipe"]["shareAs"]} rel="noopener noreferrer" target="_blank">Get Recipe</a>
         </div>
     )
 }
